f53ecdbb052b5c437b0a33a69b2e86f860e878bb
James Billingham <james@billingham.net>
2014-09-08 11:40:25 +0100

    Initial basic implementation of trying each provider - nothing clever happening for now

--- test/resilient-mailer.js
+++ test/resilient-mailer.js
@@ -18,3 +18,54 @@ test('correct types after initialization', function (t) {
 
 	t.end();
 });
+
+test('uses providers in correct order', function (t) {
+	t.plan(3);
+
+	var mailer = new ResilientMailer();
+	mailer.registerProvider(successProvider);
+	mailer.registerProvider(failureProvider);
+
+	mailer.mail({}, function (error) {
+		t.equal(typeof error, 'undefined');
+	});
+
+	var mailer2 = new ResilientMailer();
+	mailer2.registerProvider(failureProvider);
+	mailer2.registerProvider(failureProvider2);
+
+	mailer2.mail({}, function (error) {
+		t.notEqual(typeof error, 'undefined');
+		t.equal(error.message, 'different generic failure');
+	});
+});
+
+test('uses secondary as backup', function (t) {
+	t.plan(1);
+
+	var mailer = new ResilientMailer();
+	mailer.registerProvider(failureProvider);
+	mailer.registerProvider(successProvider);
+
+	mailer.mail({}, function (error) {
+		t.equal(typeof error, 'undefined');
+	});
+});
+
+var successProvider = {
+	mail: function (message, callback) {
+		callback();
+	}
+};
+
+var failureProvider = {
+	mail: function (message, callback) {
+		callback(new Error('generic failure'));
+	}
+};
+
+var failureProvider2 = {
+	mail: function (message, callback) {
+		callback(new Error('different generic failure'));
+	}
+};
